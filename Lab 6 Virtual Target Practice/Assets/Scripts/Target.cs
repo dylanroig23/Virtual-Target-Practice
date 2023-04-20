55f16ab1fd14eee81a0cd32cccfbe47f0526d356
Dylan Roig <roig.10@buckeyemail.osu.edu>
2023-04-20 17:33:13 -0400

    Targets rotation and bobbing
    targets now rotate and bob

--- Lab 6 Virtual Target Practice/Assets/Scripts/Target.cs
+++ Lab 6 Virtual Target Practice/Assets/Scripts/Target.cs
@@ -9,16 +9,22 @@ namespace RoigDylan_VukovicCharlie.Lab6
 {   
     public class Target : MonoBehaviour
     {
+        private float distanceToBob = 0.5f;
+        private float bobbingSpeed = 0.5f;
+        private Vector3 startingPosition;
+
         // Start is called before the first frame update
         void Start()
         {
-            
+            startingPosition = transform.position;
         } 
 
         // Update is called once per frame
         void Update()
         {
-            //can add some movement here if wanted
+            Vector3 distanceToMove = new Vector3(0, Mathf.Sin(Time.time * bobbingSpeed) * distanceToBob, 0);
+            transform.position = startingPosition + distanceToMove;
+            transform.Rotate(0, 50 * Time.deltaTime, 0);
         }
 
         private void OnCollisionEnter(Collision collision)
